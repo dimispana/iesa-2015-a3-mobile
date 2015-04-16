5ca40511afe0bc285e0880cc474acfae37f259eb
dimitripana <panayiotou.dimitri@gmail.com>
2015-04-16 15:02:01 +0200

    _

--- exo-07/hello/www/js/index.js
+++ exo-07/hello/www/js/index.js
@@ -37,7 +37,17 @@ var app = {
         console.log(device.cordova);
         document.getElementById('test').innerHTML = 'cordova version:'+ device.cordova + 'Cordova platform: ' + device.platform +' Cordova device : ' + device.model;
         
-        
+        var element = document.getElementById('deviceProperties');
+        element.innerHTML='<ul data-role="listview" id="list">'
+        +'<li data-icon="arrow-l>Name: ' + device.name + '</li>'
+        +'<li data-icon="arrow-r>Model: ' + device.model + '</li>'
+        +'<li data-icon="arrow-r>Cordova: ' + device.cordova + '</li>'
+        +'<li data-icon="arrow-l>Platform: ' + device.platform + '</li>'
+        +'<li data-icon="arrow-r>UUID:: ' + device.uuid + '</li>'
+        +'<li data-icon="arrow-l>Version: ' + device.version + '</li>'
+        + '</ul>'
+        ;
+     
         
     },
     // Update DOM on a Received Event
@@ -52,14 +62,4 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
-var element = document.getElementById('deviceProperties');
-element.innerHTML='<ul data-role="listview" id="list">'
-+'<li data-icon="arrow-l>Name: ' + device.name + '</li>'
-+'<li data-icon="arrow-r>Model: ' + device.model + '</li>'
-+'<li data-icon="arrow-r>Cordova: ' + device.cordova + '</li>'
-+'<li data-icon="arrow-l>Platform: ' + device.platform + '</li>'
-+'<li data-icon="arrow-r>UUID:: ' + device.uuid + '</li>'
-+'<li data-icon="arrow-l>Version: ' + device.version + '</li>'
-+ '</ul>'
-;
-app.initialize();
+   app.initialize();
